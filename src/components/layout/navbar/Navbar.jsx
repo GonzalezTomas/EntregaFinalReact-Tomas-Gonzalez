8c9fdfa759697886834cc090dffbff658249762e
GonzalezTomas <TomasG102@hotmail.com>
2023-11-30 00:18:52 -0300

    first commit

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
+// Navbar.jsx
+import React, { useState } from "react";
 import { CartWidget } from "../../common/CartWidget/CartWidget";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
@@ -7,44 +8,16 @@ import Logo from "../../../img/Logo.png";
 export const Navbar = () => {
   const [showProductList, setShowProductList] = useState(false);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const mobileMenuRef = useRef(null);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
-  const closeMobileMenu = (e) => {
-    // Si el clic ocurre dentro del menú o en el botón de hamburguesa, no cerramos el menú
-    if (
-      mobileMenuRef.current &&
-      (mobileMenuRef.current.contains(e.target) ||
-        e.target.classList.contains("hamburger-menu"))
-    ) {
-      return;
-    }
-
-    // Cerramos el menú si el clic ocurre fuera del menú o del botón de hamburguesa
-    setMobileMenuOpen(false);
-  };
-
-  // Agregamos un efecto para manejar el evento de clic en todo el documento
-  useEffect(() => {
-    document.addEventListener("touchstart", closeMobileMenu);
-
-    return () => {
-      document.removeEventListener("touchstart", closeMobileMenu);
-    };
-  }, []);
-
   return (
     <>
       <div className="navbar">
         {/* Botón de hamburguesa para dispositivos móviles */}
-        <div
-          className="hamburger-menu"
-          onClick={toggleMobileMenu}
-          onTouchStart={toggleMobileMenu} // Manejar eventos táctiles para dispositivos móviles
-        >
+        <div className="hamburger-menu" onClick={toggleMobileMenu}>
           <div className="bar"></div>
           <div className="bar"></div>
           <div className="bar"></div>
@@ -56,7 +29,7 @@ export const Navbar = () => {
 
         {/* Menú de categorías para dispositivos móviles */}
         {mobileMenuOpen && (
-          <div className="mobile-menu" ref={mobileMenuRef}>
+          <div className="mobile-menu">
             <Link to="/">Todos</Link>
             <Link to="/category/Bombones">Bombones</Link>
             <Link to="/category/Chocolates">Chocolates</Link>
